c2c8e4d35b3fdaf4b8f6a2d631ec18bf9abb79af
Omwomo <eugeneoduor79@gmail.com>
2023-07-20 18:57:30 +0300

    Refactored all the code to avoid the no-use-before-define error

--- src/index.js
+++ src/index.js
@@ -1,152 +1,16 @@
-// import _ from 'lodash';
 import './index.css';
-// import trash from './trash-can-solid.svg';
+import TaskManager from './modules/taskManager.js';
 
-// index.js
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-const taskList = document.getElementById('taskList');
-const newTaskInput = document.getElementById('newTaskInput');
-const addButton = document.getElementById('addButton');
-// const clearCompletedButton = document.getElementById('clearCompletedButton');
-
-// Function to render the tasks list
-function renderTasks() {
-  taskList.innerHTML = '';
-
-  tasks.sort((a, b) => a.index - b.index); // Sort tasks by index
-
-  tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.id = `taskList-${task.index}`; // Set the correct id for each list item
-    listItem.classList.toggle('completed', task.completed);
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
-    checkbox.addEventListener('change', () => {
-      task.completed = checkbox.checked;
-      renderTasks();
-    });
-
-    const descriptionSpan = document.createElement('span');
-    descriptionSpan.classList.add('description');
-    descriptionSpan.textContent = task.description;
-
-    // Create the edit icon
-    const editIcon = document.createElement('span');
-    editIcon.classList.add('edit-icon');
-    editIcon.textContent = '✏️';
-    // eslint-disable-next-line no-use-before-define
-    editIcon.addEventListener('click', () => editTask(task.index));
-
-    const inputElement = document.createElement('input');
-    inputElement.type = 'text';
-    inputElement.classList.add('edit-input');
-    inputElement.style.display = 'none';
-
-    const removeButton = document.createElement('span');
-    removeButton.innerHTML = '🗑️';
-    removeButton.classList.add('remove-btn');
-    // eslint-disable-next-line no-use-before-define
-    removeButton.addEventListener('click', () => removeTask(task.index));
-
-    listItem.appendChild(checkbox);
-    listItem.appendChild(descriptionSpan);
-    listItem.appendChild(editIcon);
-    listItem.appendChild(inputElement);
-    listItem.appendChild(removeButton);
-    taskList.appendChild(listItem);
-  });
-
-  // Store tasks in local storage
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Function to add a new task
-function addTask(description) {
-  const index = tasks.length > 0 ? tasks[tasks.length - 1].index + 1 : 1;
-  tasks.push({ description, completed: false, index });
-  renderTasks();
-}
-
-// Function for updating the indexes.
-function updateTaskIndexes() {
-  tasks.forEach((task, index) => {
-    task.index = index + 1;
-  });
-}
-
-// Function to remove a task
-function removeTask(index) {
-  const taskIndex = tasks.findIndex((task) => task.index === index);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-    updateTaskIndexes();
-    renderTasks();
-  }
-}
-
-// Function to edit a task
-function editTask(index) {
-  const taskIndex = tasks.findIndex((task) => task.index === index);
-  if (taskIndex !== -1) {
-    const listItem = document.getElementById(`taskList-${index}`);
-    const descriptionSpan = listItem.querySelector('.description');
-    const editIcon = listItem.querySelector('.edit-icon');
-    const inputElement = listItem.querySelector('.edit-input');
-
-    // Show the input element and set its value to the task description
-    inputElement.style.display = 'inline-block';
-    inputElement.value = tasks[taskIndex].description;
-    inputElement.focus();
-
-    // Hide the description span and edit icon during editing
-    descriptionSpan.style.display = 'none';
-    editIcon.style.display = 'none';
-
-    // Save changes when the input loses focus
-    // eslint-disable-next-line no-use-before-define
-    inputElement.addEventListener('blur', () => saveChanges(index));
-
-    inputElement.addEventListener('keypress', (event) => {
-      if (event.key === 'Enter') {
-        // eslint-disable-next-line no-use-before-define
-        saveChanges(index);
-      }
-    });
-  }
-}
-
-// Function to save changes when editing a task
-function saveChanges(index) {
-  const taskIndex = tasks.findIndex((task) => task.index === index);
-  if (taskIndex !== -1) {
-    const listItem = document.getElementById(`taskList-${index}`);
-    const descriptionSpan = listItem.querySelector('.description');
-    const editIcon = listItem.querySelector('.edit-icon');
-    const inputElement = listItem.querySelector('.edit-input');
-
-    tasks[taskIndex].description = inputElement.value.trim();
-    inputElement.style.display = 'none';
-    descriptionSpan.textContent = tasks[taskIndex].description;
-    descriptionSpan.style.display = 'inline-block';
-    editIcon.style.display = 'inline-block';
-
-    renderTasks();
-  }
-}
+const taskManager = new TaskManager();
 
 // Event listener for the add button
-addButton.addEventListener('click', () => {
-  const newTaskDescription = newTaskInput.value.trim();
+document.getElementById('addButton').addEventListener('click', () => {
+  const newTaskDescription = document.getElementById('newTaskInput').value.trim();
   if (newTaskDescription) {
-    addTask(newTaskDescription);
-    newTaskInput.value = '';
+    taskManager.addTask(newTaskDescription);
+    document.getElementById('newTaskInput').value = '';
   }
 });
 
-// Event listener for the clear completed button
-// clearCompletedButton.addEventListener('click', clearCompletedTasks);
 // Initial rendering of the tasks
-renderTasks();
+taskManager.renderTasks();
