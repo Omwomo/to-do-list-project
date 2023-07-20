d9146c9e57cfa2dcea37045991c7baba2740a1d8
Omwomo <eugeneoduor79@gmail.com>
2023-07-20 09:29:01 +0300

    Render tasks to the screen and save them to local storage

--- src/index.js
+++ src/index.js
@@ -13,17 +13,13 @@ function component() {
 
 document.body.appendChild(component());
 
-// Sample data for the tasks array
-const tasks = [
-  { description: 'Task 1', completed: false, index: 1 },
-  { description: 'Task 2', completed: true, index: 2 },
-  // Add more tasks here as needed
-];
+// index.js
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const taskList = document.getElementById('taskList');
-// const newTaskInput = document.getElementById('newTaskInput');
-// const addButton = document.getElementById('addButton');
-// const clearCompletedButton = document.getElementById('clearCompletedButton');
+const newTaskInput = document.getElementById('newTaskInput');
+const addButton = document.getElementById('addButton');
+const clearCompletedButton = document.getElementById('clearCompletedButton');
 
 // Function to render the tasks list
 function renderTasks() {
@@ -33,23 +29,45 @@ function renderTasks() {
 
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
+    listItem.classList.toggle('completed', task.completed);
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = task.completed;
+    checkbox.addEventListener('change', () => {
+      task.completed = checkbox.checked;
+      renderTasks();
+    });
 
     const descriptionSpan = document.createElement('span');
+    descriptionSpan.classList.add('description'); // Add class for the description span
     descriptionSpan.textContent = task.description;
+    // eslint-disable-next-line no-use-before-define
+    // descriptionSpan.addEventListener('click', () => editTask(task.index));
+
+    const inputElement = document.createElement('input');
+    inputElement.type = 'text';
+    inputElement.classList.add('edit-input');
+    inputElement.style.display = 'none';
+    // eslint-disable-next-line no-use-before-define
+    // inputElement.addEventListener('blur', saveChanges);
+    // eslint-disable-next-line no-use-before-define
+    // inputElement.addEventListener('keypress', handleKeyPress);
 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
     // eslint-disable-next-line no-use-before-define
-    // removeButton.addEventListener('click', () => removeTask(task.index));
+    removeButton.addEventListener('click', () => removeTask(task.index));
 
     listItem.appendChild(checkbox);
     listItem.appendChild(descriptionSpan);
+    listItem.appendChild(inputElement);
     listItem.appendChild(removeButton);
     taskList.appendChild(listItem);
   });
+
+  // Store tasks in local storage
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Initial rendering of the tasks
